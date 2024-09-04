75dd101ebebffc37c1215d60f51816f315d6a25a
Lucas Anthero de Paula Fernandes <lucasanthero.psico@gmail.com>
2024-09-03 23:43:04 -0300

    mudanças site le

---
+++ scripts/javas.js
@@ -0,0 +1,17 @@
+function menu(){
+    if(nave.style.display == 'block'){
+        nave.style.display = 'none'
+    }
+    else{
+        nave.style.display = 'block'
+    }
+}
+
+function tatela(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    }
+    else{
+        itens.style.display = 'none'
+    }
+}
